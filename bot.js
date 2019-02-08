baf7e320a7d4140c47680b77e9b2e18a8dbfa3be
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2019-02-09 00:22:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js")
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("462635240825815050");
+const mohamed= member.guild.channels.get("542045396986232842");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`** Welcome To Vision Omg Family ..** `), 4000)        
+setTimeout(() => mohamed.send(`** Welcome To Server Mario Family** `), 4000)        
 }
 });
 
